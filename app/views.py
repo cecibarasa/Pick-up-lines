44554409d7feee1b62aaa8019d6c77edab1dcf33
Cecilia Barasa <cecibarasa@gmail.com>
2020-06-06 07:24:50 +0300

    add jinja template

--- app/views.py
+++ app/views.py
@@ -8,4 +8,13 @@ def index():
     View root page function that returns index page and its data
     '''
 
-    return render_template('index.html')
+    message = 'Pitches'
+    return render_template('index.html', message=message)
+    
+@app.route('/pitch/<int:pitch_id')
+def pitch(pitch_id):
+    '''
+    view pitch function that returns pitch categories
+    '''
+
+    return render_template('pitch.html', id = pitch_id)    
