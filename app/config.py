c6458fc9d7d1e9de456421b900e4fa7245f7c538
Cecilia Barasa <cecibarasa@gmail.com>
2020-06-06 07:28:17 +0300

    add cconfig classes

---
+++ app/config.py
@@ -0,0 +1,20 @@
+class Config:
+    '''
+    general parent class
+    '''
+
+    pass
+
+class ProdConfig:
+    '''
+    production config child class
+    '''
+
+    pass
+
+class DevConfig:
+    '''
+    development config class
+    '''
+
+    DEBUG = True
