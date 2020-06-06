55b47beb87ff730cf7f1f3e91207c72251b3c0f0
Cecilia Barasa <cecibarasa@gmail.com>
2020-06-06 08:00:00 +0300

    add error file

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 #Initializing application
 app = Flask(__name__)
 
 #setting up configuration
 app.config.from_object(DevConfig)
+
+#initializing Flask extensions
+bootstrap = Bootstrap(app)
 
 from app import views
