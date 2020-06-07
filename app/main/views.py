b1c41a3feed9a4e548700b58e942c02f0075a212
Cecilia Barasa <cecibarasa@gmail.com>
2020-06-07 06:48:23 +0300

    add flask uploads

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort
 from . import main
+from ..models import User,Pitch,Comment,Upvote,Downvote
+from .form import UpdateProfile,PitchForm,CommentForm
 
 #Views
 @main.route('/')
@@ -7,9 +9,14 @@ def index():
     '''
     View root page function that returns index page and its data
     '''
+    pithes = Pitch.query.all()
+    pickuplines = Pitch.query.filter_by(category='Pick up lines').all()
+    productpitch = Pitch.query.filter_by(category='Product Pitch').all()
+    interviewpitch = Pitch.query.filter_by(category='Interview Pitch').all()
+    promotionpitch = Pitch.query.filter_by(category='Promotion Pitch').all()
 
     title = 'Home - Welcome to The Pitches of The Century'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title, pickuplines = pickuplines,productpitch = productpitch,interviewpitch = interviewpitch,promotionpitch = promotionpitch)
     
 @main.route('/pitch', methods=['GET', 'POST'])
 @login_required
@@ -17,5 +24,25 @@ def pitch():
     '''
     view pitch function that returns pitch categories
     '''
+    form = PitchForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        post = form.post.data
+        category = form.category.data
+        user_id = current
 
-    return render_template('pitch.html')    
+    return render_template('pitch.html')
+
+
+@main.route('/user/<name>/updateprofile', methods = ['POST','GET'])
+@login_required
+def updateprofile(name):
+    form = UpdateProfile()
+    user = User.query.filter_by(username = name).first()
+    if user == None:
+        abort(404)
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        user.save_u()
+        return redirect(url_for('.profile',name = name))
+    return render_template('profile/update.html',form =form)       
