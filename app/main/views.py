1c74ccede426b8d9e17b95fcf7869882e2d63d42
Cecilia Barasa <cecibarasa@gmail.com>
2020-06-07 15:09:46 +0300

    change redirect

--- app/main/views.py
+++ app/main/views.py
@@ -36,32 +36,38 @@ def pitch():
         new_pitch_object.save_p()
         return redirect(url_for('main.index'))
 
-    return render_template('pitch.html', form = form)
+    return render_template('pitch.html', form=form)
+    
+@main.route('/user/<name>')
+def profile(name):
+    user = User.query.filter_by(username = name).first()
+    user_id = current_user._get_current_object().id
+    posts = Pitch.query.filter_by(user_id = user_id).all()
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user,posts=posts)    
 
 
 @main.route('/user/<name>/updateprofile', methods = ['POST','GET'])
 @login_required
 def updateprofile(name):
-    form = UpdateProfile()
     user = User.query.filter_by(username = name).first()
-    if user == None:
+    if user is None:
         abort(404)
+
+    form = UpdateProfile()
+
     if form.validate_on_submit():
         user.bio = form.bio.data
-        user.save_u()
-        return redirect(url_for('.profile',name = name))
-    return render_template('profile/update.html', form=form)
 
-# @main.route('/user/<uname>/update/pic',methods= ['POST'])
-# @login_required
-# def update_pic(uname):
-#     user = User.query.filter_by(username = uname).first()
-#     if 'photo' in request.files:
-#         filename = photos.save(request.files['photo'])
-#         path = f'photos/{filename}'
-#         user.profile_pic_path = path
-#         db.session.commit()
-#     return redirect(url_for('main.profile', uname=uname))
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',name=user.username))
+
+    return render_template('profile/update.html',form =form)
+
 
 @main.route('/comment/<int:pitch_id>', methods = ['POST','GET'])
 @login_required
@@ -79,46 +85,36 @@ def comment(pitch_id):
     return render_template('comment.html', form =form, pitch = pitch,all_comments=all_comments)
 
 
-@main.route('/user/<name>')
-def profile(name):
+# @main.route('/user/<name>/update',methods = ['GET','POST'])
+# @login_required
+# def update_profile(name):
+#     user = User.query.filter_by(username = name).first()
+#     if user is None:
+#         abort(404)
+
+#     form = UpdateProfile()
+
+#     if form.validate_on_submit():
+#         user.bio = form.bio.data
+
+#         db.session.add(user)
+#         db.session.commit()
+
+#         return redirect(url_for('.profile',name=user.username))
+
+#     return render_template('profile/update.html',form =form)
+
+
+@main.route('/user/<name>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(name):
     user = User.query.filter_by(username = name).first()
-    user_id = current_user._get_current_object().id
-    posts = Pitch.query.filter_by(user_id = user_id).all()
-    if user is None:
-        abort(404)
-
-    return render_template("profile/profile.html", user = user,posts=posts)
-
-@main.route('/user/<uname>/update',methods = ['GET','POST'])
-@login_required
-def update_profile(uname):
-    user = User.query.filter_by(username = uname).first()
-    if user is None:
-        abort(404)
-
-    form = UpdateProfile()
-
-    if form.validate_on_submit():
-        user.bio = form.bio.data
-
-        db.session.add(user)
-        db.session.commit()
-
-        return redirect(url_for('.profile',uname=user.username))
-
-    return render_template('profile/update.html',form =form)
-
-
-@main.route('/user/<uname>/update/pic',methods= ['POST'])
-@login_required
-def update_pic(uname):
-    user = User.query.filter_by(username = uname).first()
     if 'photo' in request.files:
         filename = photos.save(request.files['photo'])
         path = f'photos/{filename}'
         user.profile_pic_path = path
         db.session.commit()
-    return redirect(url_for('main.profile',uname=uname))
+    return redirect(url_for('main.profile',name=name))
 
 @main.route('/like/<int:id>',methods = ['POST','GET'])
 @login_required
